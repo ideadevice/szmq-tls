1ef0902f94228ef099ec412b46ad0348b98fac1d
Harshvardhan Sharma <harsh@ideadevice.com>
2013-07-03 10:04:07 +0000

    added comments

--- src/szmq.c
+++ src/szmq.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <zmq.h>
-#include <assert.h>
 #include <errno.h>
 #include <gnutls/gnutls.h>
 #include "szmq.h"
@@ -20,8 +19,11 @@ void szmq_global_init (szmq_context *szmq_ctx)
 
   gnutls_certificate_set_dh_params (szmq_ctx->credentials, szmq_ctx->dh_params);
 
+  /* set gnutls priority priority string 
+   call this function from your program to set a different value */
   gnutls_priority_init (&(szmq_ctx->priority), "NORMAL", NULL);
 
+  /* set callback function to verify peer's certificate */
   gnutls_certificate_set_verify_function (szmq_ctx->credentials, _verify_certificate_callback);
 
 }
@@ -44,15 +46,18 @@ int szmq_set_crl_file (szmq_context *szmq_ctx, const char *crlfile,unsigned int 
 					      crlfile, flags);
 }
 
-void szmq_session_init (szmq_context *szmq_ctx, szmq_session  *session,			      void *socket, unsigned int flags)
+/* Initialize szmq session.
+This function initializes the gnutls session, sets up the required callbacks. */
+void szmq_session_init (szmq_context *szmq_ctx, szmq_session  *session, void *socket, unsigned int flags)
 {
   session = (szmq_session *) malloc (sizeof(szmq_session));
   session->transport.pos = 0;
   session->transport.size = 0;
   session->transport.sending = 0;
   session->transport.zmq_flags = 0;
-  session->transport.socket = socket;
+  session->transport.socket = socket;	/* zmq socket */
   session->type = flags;
+  /* init gnutls session */
   gnutls_init (&(session->gnutls_session), flags);
 
   gnutls_transport_set_ptr ((session->gnutls_session), &(session->transport));
@@ -63,29 +68,34 @@ void szmq_session_init (szmq_context *szmq_ctx, szmq_session  *session,			      
 
   gnutls_transport_set_pull_timeout_function ((session->gnutls_session), z_timeout);
 
+  /* cipher suite priority for gnutls */
   gnutls_priority_set ((session->gnutls_session), szmq_ctx->priority);
 
   gnutls_credentials_set ((session->gnutls_session), GNUTLS_CRD_CERTIFICATE, szmq_ctx->credentials);
 
 }
 
+/* set last argument for the next zmq_send or zmq_recv call */
 void szmq_set_flag(szmq_session *session, unsigned int flag)
 {
   session->transport.zmq_flags = flag;
 }
 
+/* perform the TLS handshake */
 int szmq_handshake (szmq_session *session, int timeout)
 {
   int ret;
   gnutls_transport_set_push_function ((session->gnutls_session), z_send_handshake);
-
   gnutls_transport_set_pull_function ((session->gnutls_session), z_recv_handshake);
   gnutls_handshake_set_timeout(session->gnutls_session, timeout);
   zmq_pollitem_t item [] = { {session->transport.socket, 0, ZMQ_POLLIN | ZMQ_POLLOUT, 0} };
   do
   {
+    /* In case of a server, poll the socket for incoming or outgoing messages.
+      If there are unsent messages in zmq buffer this will continue the handshake without blocking.*/
     if(session->type & GNUTLS_SERVER) {
       if(zmq_poll(item, 1, -1) == 1) ret = gnutls_handshake (session->gnutls_session);
+      /* retry without clearing the buffers for E_AGAIN and E_INTERRUPTED */
       if(ret != GNUTLS_E_AGAIN && ret != GNUTLS_E_INTERRUPTED) z_clear_buffer_server(session);
     }
     else {
@@ -99,13 +109,14 @@ int szmq_handshake (szmq_session *session, int timeout)
     fprintf (stderr, "*** Handshake has failed (%s)\n\n",
 	    gnutls_strerror (ret));
   }
+  /* after the handshake is over change to strictly synchronous callbacks */
   gnutls_transport_set_push_function ((session->gnutls_session), z_send);
 
   gnutls_transport_set_pull_function ((session->gnutls_session), z_recv);
   return ret;
 }
 
-
+/* Sends the message on the zmq socket after encrypting */
 int szmq_send(szmq_session *session,void *buf, size_t len)
 {
   int ret;
@@ -114,6 +125,7 @@ int szmq_send(szmq_session *session,void *buf, size_t len)
   return ret;
 }
 
+/* Receives and decrypts message on the zmq socket */
 int szmq_recv(szmq_session *session, void *buf, size_t len)
 {
   int ret;
@@ -122,12 +134,14 @@ int szmq_recv(szmq_session *session, void *buf, size_t len)
   return ret;
 }
 
+/* free the szmq session including the gnutls session */
 void szmq_session_deinit(szmq_session *session)
 {
   gnutls_deinit(session->gnutls_session);
   free(session);
 }
 
+/* destroy szmq context and deinitialize gnutls global structs */
 void szmq_global_deinit(szmq_context *ctx)
 {
   gnutls_certificate_free_credentials(ctx->credentials);
@@ -137,7 +151,7 @@ void szmq_global_deinit(szmq_context *ctx)
   gnutls_global_deinit();
 }
 
-
+/* close the szmq session */
 int szmq_bye (szmq_session *session)
 {
   return gnutls_bye(session->gnutls_session, GNUTLS_SHUT_WR);
--- src/szmq_callback.c
+++ src/szmq_callback.c
@@ -23,6 +23,7 @@ generate_dh_params (gnutls_dh_params_t *dh_params)
   return 0;
 }
 
+/* verify peer's certificate */
 static int
 _verify_certificate_callback (gnutls_session_t session)
 {
@@ -32,15 +33,17 @@ _verify_certificate_callback (gnutls_session_t session)
   const char *hostname;
   gnutls_datum_t out;
 
-  /* read hostname */
+  /* read hostname 
+    This must be set using gnutls_session_set_ptr() */
   hostname = gnutls_session_get_ptr (session);
+
   /* This verification function uses the trusted CAs in the credentials
    * structure. So you must have installed one or more CA certificates.
    */
   ret = gnutls_certificate_verify_peers3 (session, hostname, &status);
   if (ret < 0)
     {
-      printf ("Error\n");
+      fprintf (stderr, "Certificate Error\n");
       return GNUTLS_E_CERTIFICATE_ERROR;
     }
 
@@ -49,11 +52,11 @@ _verify_certificate_callback (gnutls_session_t session)
   ret = gnutls_certificate_verification_status_print( status, type, &out, 0);
   if (ret < 0)
     {
-      printf ("Error\n");
+      fprintf (stderr, "Certificate Error\n");
       return GNUTLS_E_CERTIFICATE_ERROR;
     }
   
-  printf ("%s", out.data);
+  fprintf (stderr, "%s\n", out.data);
   
   gnutls_free(out.data);
 
@@ -64,7 +67,7 @@ _verify_certificate_callback (gnutls_session_t session)
   return 0;
 }
 
-// Push function to be used during gnutls handshake
+/* Push function to be used during gnutls handshake */
 ssize_t z_send_handshake(gnutls_transport_ptr_t transport, const void* buf,size_t len )
 {
   szmq_transport *tr;
@@ -72,16 +75,17 @@ ssize_t z_send_handshake(gnutls_transport_ptr_t transport, const void* buf,size_
   if(SZMQ_DEBUG_FUNCTION_CALLS) fprintf(stderr,"handshake sending...\n");
   tr->sending = 1;
 
-  // During TLS handshake, we may need to send multiple messages before receiving. 
-  // Send them as multipart messages to avoid EFSM on req-rep sockets. Send a delimiter frame later before receiving.
+  /* During TLS handshake, we may need to send multiple messages before receiving. 
+   Send them as multipart messages to avoid EFSM on req-rep sockets. 
+   Send a delimiter frame later before receiving. */
   int rc = zmq_send(tr->socket, buf, len, ZMQ_SNDMORE);
   if(rc < 0) perror("send error");
   else if(SZMQ_DEBUG_PUSH_PULL) fprintf(stderr,"Message queued %d\n",rc);
   return rc;
 }
 
-// Push function to be used after handshake is over.
-// Follows request-reply mode strictly.
+/* Push function to be used after handshake is over.
+ Follows request-reply mode strictly. */
 ssize_t z_send(gnutls_transport_ptr_t transport, const void* buf,size_t len )
 {
   szmq_transport *tr;
@@ -93,7 +97,7 @@ ssize_t z_send(gnutls_transport_ptr_t transport, const void* buf,size_t len )
   return rc;
 }
 
-// Helper function for reading messages from ZMQ socket
+/* Helper function for reading messages from ZMQ socket */
 int z_recv_socket(gnutls_transport_ptr_t transport)
 {
   szmq_transport *tr;
@@ -116,26 +120,26 @@ int z_recv_socket(gnutls_transport_ptr_t transport)
     return size;
 }
 
-// Pull function to be used during gnutls handshake
+/* Pull function to be used during gnutls handshake */
 ssize_t z_recv_handshake(gnutls_transport_ptr_t transport, void* buf, size_t len)
 {
   szmq_transport *tr;
   tr= (szmq_transport *)transport;
   if(SZMQ_DEBUG_FUNCTION_CALLS) fprintf(stderr,"handshake receiving...\n");
 
-  // If buffer is empty, read data from ZMQ socket
+  /* If buffer is empty, read data from ZMQ socket */
   if(tr->size == 0) {
     tr->size = z_recv_socket(tr);
     if(tr->size < 0) return -1;
   }
 
-  // Copy the portion of the received message required by gnutls
+  /* Copy the portion of the received message required by gnutls */
   memcpy(buf, (void *)(tr->recv_buf + tr->pos), len);
   if(SZMQ_DEBUG_PUSH_PULL) fprintf(stderr, "Read message from buffer %d\n",len);
   tr->pos += len;
   tr->size -= len;
 
-  // If buffer is empty receive the next message part or the delimiter.
+  /* If buffer is empty receive the next message part or the delimiter. */
   if(tr->size == 0) {
     tr->size = z_recv_socket(tr);
     if(tr->size < 0) return -1;
@@ -143,21 +147,21 @@ ssize_t z_recv_handshake(gnutls_transport_ptr_t transport, void* buf, size_t len
   return len;
 }
 
-// Pull function to be used after handshake is over.
-// Fully synchronous
+/* Pull function to be used after handshake is over.
+   follws request-reply mode strictly */
 ssize_t z_recv(gnutls_transport_ptr_t transport, void* buf, size_t len)
 {
   szmq_transport *tr;
   tr= (szmq_transport *)transport;
   if(SZMQ_DEBUG_FUNCTION_CALLS) fprintf(stderr,"receiving...\n");
 
-  // If buffer is empty, read data from ZMQ socket
+  /* If buffer is empty, read data from ZMQ socket */
   if(tr->size == 0 ) {
     tr->size = z_recv_socket(tr);
     if(tr->size < 0) return -1;
   }
 
-  // Copy the portion required by gnutls
+  /* Copy the portion required by gnutls */
   memcpy(buf,(void *)(tr->recv_buf + tr->pos), len);
   if(SZMQ_DEBUG_PUSH_PULL) fprintf(stderr, "Read message from buffer %d\n",len);
   tr->pos += len;
@@ -165,37 +169,37 @@ ssize_t z_recv(gnutls_transport_ptr_t transport, void* buf, size_t len)
   return len;
 }
 
-// Clear server buffer after handshake
+/* Clear server buffer after handshake */
 void z_clear_buffer_server(szmq_session *session)
 {
   if(SZMQ_DEBUG_FUNCTION_CALLS) fprintf(stderr,"clearing buffer...\n");
   session->transport.pos = 0;
   session->transport.size = 0;
 
-  // Discard any unread messages from a failed handshake
+  /* Discard any unread messages from a failed handshake */
   zmq_pollitem_t item [] = { {session->transport.socket, 0, ZMQ_POLLIN, 0} };
   while(zmq_poll(item, 1, 0) == 1) {
     zmq_recv(session->transport.socket, session->transport.recv_buf, RECV_BUFSIZE, 0);
     if(SZMQ_DEBUG_PUSH_PULL) fprintf(stderr, "discarded message\n");
   }
 
-  // Send delimiter frame and change the socket state to receive
+  /* Send delimiter frame and change the socket state to receive */
     zmq_send(session->transport.socket, "", 0, ZMQ_DONTWAIT);
     session->transport.sending = 0;
     if(SZMQ_DEBUG_PUSH_PULL) fprintf(stderr, "Queued messages sent\n");
 }
 
-// Poll function
+/* Poll function */
 int z_timeout(gnutls_transport_ptr_t transport, unsigned int time)
 {
   szmq_transport *tr;
   tr= (szmq_transport *)transport;
   if(SZMQ_DEBUG_FUNCTION_CALLS) fprintf(stderr, "timeout function...\n");
 
-  // Do not poll if buffer is not empty 
+  /* Do not poll if buffer is not empty */
   if(tr->size != 0 ) return 1;
 
-  // Send queued messages before polling
+  /* Send queued messages before polling */
   if(tr->sending) {
     int a = zmq_send(tr->socket, "", 0, 0);
     if(a>=0 && SZMQ_DEBUG_PUSH_PULL) fprintf(stderr, "Queued messages sent\n");
@@ -204,7 +208,7 @@ int z_timeout(gnutls_transport_ptr_t transport, unsigned int time)
 
   if(SZMQ_DEBUG_PUSH_PULL) fprintf(stderr,"polling...\n");
 
-  // Poll the socket for incoming messages
+  /* Poll the socket for incoming messages */
   zmq_pollitem_t item;
   item.socket = tr->socket;
   item.events = ZMQ_POLLIN;
