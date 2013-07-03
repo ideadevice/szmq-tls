b3f2eabd2dcfbc3e8a6ff50b8b91f88e27622d59
Harshvardhan Sharma <harsh@ideadevice.com>
2013-07-03 07:59:51 +0000

    changed struct definitions

--- include/szmq_callback.h
+++ include/szmq_callback.h
@@ -1,6 +1,8 @@
 #ifndef __SZMQ_CALLBACK_H_INCLUDED__
 #define __SZMQ_CALLBACK_H_INCLUDED__
 
+#include <gnutls/gnutls.h>
+
 static int generate_dh_params (gnutls_dh_params_t *dh_params);
 
 static int _verify_certificate_callback (gnutls_session_t session);
@@ -15,7 +17,7 @@ ssize_t z_recv_handshake(gnutls_transport_ptr_t socket, void* buf, size_t len);
 
 ssize_t z_recv(gnutls_transport_ptr_t socket, void* buf, size_t len);
 
-void z_clear_buffer_server(void* socket, int ret);
+void z_clear_buffer_server(szmq_session *session);
 
 int z_timeout(gnutls_transport_ptr_t socket, unsigned int time);
 
